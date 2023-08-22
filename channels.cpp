71bbf300cbde56602a9e54fb14c98d1624d117a2
Prince Patel <prince.patel.14@icloud.com>
2023-08-22 18:06:12 +0530

    separate channels for consumer and producer

--- channels.cpp
+++ channels.cpp
@@ -5,54 +5,83 @@
 #include <queue>
 #include <optional>
 #include <condition_variable>
+#include <variant>
+#include <vector>
 
 template<typename T>
 class Channel {
-    static constexpr std::size_t default_max_length = 30;
-  public:
-    using value_type = T;
+		static constexpr std::size_t default_max_length = 30;
+public:
+		using value_type = T;
 
-    explicit Channel(std::size_t max_length = default_max_length)
-      : max_length_(max_length) {}
+		explicit Channel(std::size_t max_length = default_max_length, std::size_t vector_size = 1)
+				: max_length_(max_length), vector_size_(vector_size), use_vector_(vector_size > 1) {
+						if (use_vector_) {
+								buffer_ = std::vector<std::queue<value_type>>(vector_size_);
+						}
+				}
 
-    std::optional<value_type> next() {
-        std::unique_lock locker(mtx_);
-        cond_.wait(locker, [this]() {
-            return !buffer_.empty() || closed_; 
-        });
-        
-        if (buffer_.empty()) {
-          assert(closed_);
-          return std::nullopt;
-        }
-        
-        value_type val = buffer_.top().second;
-        buffer_.pop();
-        cond_.notify_one();
-        return val;
-    }
-    
-    void put(int id, value_type val) {
-        std::unique_lock locker(mtx_);
-        cond_.wait(locker, [this]() {
-            return buffer_.size() < max_length_;
-        });
+		std::optional<value_type> next(int id) {
+				std::unique_lock locker(mtx_);
+				cond_.wait(locker, [this, id]() {
+						return !empty(id) || closed_;
+				});
 
-        buffer_.push(std::make_pair(id, std::move(val)));
-        cond_.notify_one();
-    }
+				if (empty(id)) {
+						assert(closed_);
+						return std::nullopt;
+				}
 
-    void close() {
-        std::scoped_lock locker(mtx_);
-        closed_ = true;
-        cond_.notify_all();
-    }
+				value_type val = front(id);
+				pop(id);
+				cond_.notify_one();
+				return val;
+		}
 
-  private:
-    std::size_t max_length_;  
-    std::priority_queue<std::pair<int, value_type>, std::vector<std::pair<int, value_type>>, std::greater<>> buffer_;
-    bool closed_ = false;    
+		void put(int id, value_type val) {
+				std::unique_lock locker(mtx_);
+				cond_.wait(locker, [this, id]() {
+						return size(id) < max_length_;
+				});
 
-    std::mutex mtx_;
-    std::condition_variable cond_; 
+				push(id, std::move(val));
+				cond_.notify_one();
+		}
+
+		void close() {
+				std::scoped_lock locker(mtx_);
+				closed_ = true;
+				cond_.notify_all();
+		}
+
+private:
+		std::size_t max_length_;
+		std::size_t vector_size_;
+		bool use_vector_;
+		std::variant<std::queue<value_type>, std::vector<std::queue<value_type>>> buffer_;
+		bool closed_ = false;
+
+		std::mutex mtx_;
+		std::condition_variable cond_;
+
+		// Helper functions to operate on the buffer variant
+		bool empty(int id) const {
+				return std::visit([id](const auto& b) { return b[id].empty(); }, buffer_);
+		}
+
+		std::size_t size(int id) const {
+				return std::visit([id](const auto& b) { return b[id].size(); }, buffer_);
+		}
+
+		value_type front(int id) const {
+				return std::visit([id](const auto& b) { return b[id].front(); }, buffer_);
+		}
+
+		void pop(int id) {
+				std::visit([id](auto& b) { b[id].pop(); }, buffer_);
+		}
+
+		void push(int id, value_type val) {
+				std::visit([id, &val](auto& b) { b[id].push(std::move(val)); }, buffer_);
+		}
 };
--- main.cpp
+++ main.cpp
@@ -1,43 +1,41 @@
 #include "channels.cpp"
 
 
-void producer_main(Channel<int>& chan, int val, int producer_id) {
-	while (val >= 0) {
-		chan.put(val, val);
-		std::cout << "Produced " << val << " by " << producer_id << std::endl;
-		val--;
-	}
+void producer_main(Channel<int>& chan, int val, int producer_id, int queue_id) {
+    while (val >= 0) {
+        chan.put(queue_id, val);
+        std::cout << "Produced " << val << " by producer " << producer_id << " to queue " << queue_id << std::endl;
+        val--;
+    }
 }
 
-void consumer_main(Channel<int>& chan, int consumer_id) {
-	bool running = true;
-	while (running) {
-		auto val = chan.next();
-		if (!val) {
-			running = false;
-			continue;
-		}
+void consumer_main(Channel<int>& chan, int queue_id) {
+    bool running = true;
+    while (running) {
+        auto val = chan.next(queue_id);
+        if (!val) {
+            running = false;
+            continue;
+        }
 
-		std::cout << "Consumed " << *val << " by " << consumer_id << std::endl;
-	};
+        std::cout << "Consumed " << *val << " from queue " << queue_id << std::endl;
+    };
 }
-
 
 int main()
 {
-	Channel<int> channel;
+	Channel<int> channel_with_vector(30, 3);
 
-	std::thread producer_thread1(producer_main, std::ref(channel), 10, 1);
-	std::thread producer_thread2(producer_main, std::ref(channel), 10, 2);
-	
-	std::thread consumer_thread(consumer_main, std::ref(channel), 1);
-	
-	producer_thread1.join();
-	producer_thread2.join();
-	channel.close();
-	
-	consumer_thread.join();
-	
+    std::thread producer_thread1(producer_main, std::ref(channel_with_vector), 10, 1, 0);
+    std::thread producer_thread2(producer_main, std::ref(channel_with_vector), 10, 2, 1);
 
-	return 0;
+    std::thread consumer_thread1(consumer_main, std::ref(channel_with_vector), 0);
+    std::thread consumer_thread2(consumer_main, std::ref(channel_with_vector), 1);
+
+    producer_thread1.join();
+    producer_thread2.join();
+    channel_with_vector.close();
+
+    consumer_thread1.join();
+    consumer_thread2.join();
 }
