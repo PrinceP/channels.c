f90ac7f9718380590966f17db82229bdda4ed272
Prince Patel <prince.patel.14@icloud.com>
2023-08-22 14:03:24 +0530

    priority queue used

--- channels.cpp
+++ channels.cpp
@@ -8,7 +8,7 @@
 
 template<typename T>
 class Channel {
-    static constexpr std::size_t default_max_length = 10;
+    static constexpr std::size_t default_max_length = 30;
   public:
     using value_type = T;
 
@@ -26,19 +26,19 @@ class Channel {
           return std::nullopt;
         }
         
-        value_type val = buffer_.front();
+        value_type val = buffer_.top().second;
         buffer_.pop();
         cond_.notify_one();
         return val;
     }
     
-    void put(value_type val) {
+    void put(int id, value_type val) {
         std::unique_lock locker(mtx_);
         cond_.wait(locker, [this]() {
             return buffer_.size() < max_length_;
         });
 
-        buffer_.push(std::move(val));
+        buffer_.push(std::make_pair(id, std::move(val)));
         cond_.notify_one();
     }
 
@@ -50,10 +50,9 @@ class Channel {
 
   private:
     std::size_t max_length_;  
-    std::queue<value_type> buffer_;
+    std::priority_queue<std::pair<int, value_type>, std::vector<std::pair<int, value_type>>, std::greater<>> buffer_;
     bool closed_ = false;    
 
     std::mutex mtx_;
     std::condition_variable cond_; 
 };
-
--- main.cpp
+++ main.cpp
@@ -1,37 +1,43 @@
 #include "channels.cpp"
 
-void producer_main(Channel<int>& chan, int val) {
-    while (val >= 0) {
-        chan.put(val);
-        std::cout << "Produced " << val << std::endl;
-        val--;
-    }
+
+void producer_main(Channel<int>& chan, int val, int producer_id) {
+	while (val >= 0) {
+		chan.put(val, val);
+		std::cout << "Produced " << val << " by " << producer_id << std::endl;
+		val--;
+	}
 }
 
-void consumer_main(Channel<int>& chan) {
-    bool running = true;
-    while (running) {
-        auto val = chan.next();
-        if (!val) {
-            running = false;
-            continue;
-        }
+void consumer_main(Channel<int>& chan, int consumer_id) {
+	bool running = true;
+	while (running) {
+		auto val = chan.next();
+		if (!val) {
+			running = false;
+			continue;
+		}
 
-        std::cout << "Consumed " << *val << std::endl;
-    };
+		std::cout << "Consumed " << *val << " by " << consumer_id << std::endl;
+	};
 }
+
 
 int main()
 {
-    Channel<int> channel;
+	Channel<int> channel;
 
-    std::thread producer_thread(producer_main, std::ref(channel), 13);
-    std::thread consumer_thread(consumer_main, std::ref(channel));
-    
-    producer_thread.join();
-    channel.close();
+	std::thread producer_thread1(producer_main, std::ref(channel), 10, 1);
+	std::thread producer_thread2(producer_main, std::ref(channel), 10, 2);
+	
+	std::thread consumer_thread(consumer_main, std::ref(channel), 1);
+	
+	producer_thread1.join();
+	producer_thread2.join();
+	channel.close();
+	
+	consumer_thread.join();
+	
 
-    consumer_thread.join();
-
-    return 0;
+	return 0;
 }
